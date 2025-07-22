cf17bb7dbf05f64f0eb7015d59b53af8a16ef285
Kjell Heinsen <ich@kjell-heinsen.de>
2025-07-22 23:51:32 +0200

    core formvalidation

---
+++ hit-cookiemaster/app/classes/core/formvalidator.php
@@ -0,0 +1,16 @@
+<?php
+namespace HITCookieMaster\App\Classes\Core;
+
+/**
+ * Klasse für die Formular-Validierung und Sicherheitsüberprüfungen
+ *
+ * Diese Klasse bietet zentrale Funktionen für die Validierung von Formularen,
+ * insbesondere die Überprüfung von nonce-Tokens und Benutzerberechtigungen.
+ * Sie kann in allen Formularen des Plugins verwendet werden.
+ */
+class FormValidator {
+
+
+    // TODO Verify Nonce , Verify Ajax Requests, nonce_field & other validation
+
+}
